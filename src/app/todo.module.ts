b01d92a60d25fc884529f136ee43c99d9b4b6688
Anandakumar Palanisamy <ananda11k@gmail.com>
2017-08-28 02:39:31 +0530

    Add Todo Component html and styling

--- src/app/todo.module.ts
+++ src/app/todo.module.ts
@@ -1,11 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { MdToolbarModule } from '@angular/material';
+import { MdCardModule } from '@angular/material';
+import { MdInputModule } from '@angular/material';
+import { MdButtonModule } from '@angular/material';
 import { TodoComponent} from './todo.component';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    MdCardModule,
+    MdInputModule,
+    MdButtonModule
   ],
   declarations: [
     TodoComponent
